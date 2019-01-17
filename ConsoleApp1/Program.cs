a4c4769761f46fe3bb47003cbac80662b8a1e175
Jamel Warren <jamnwarr@ut.utm.edu>
2019-01-17 11:57:47 -0600

    This is a guessing function.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,24 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("I am thinking of a number between 0 and 100. What is it?");
+            Random rnd = new Random();
+            int x = rnd.Next(0, 100);
+            string y = Console.ReadLine();
+            int z = Convert.ToInt32(y);
+            while (x != z)
+            {
+
+
+                if (x > z)
+                    Console.WriteLine("This is too LOW!");
+                else
+                    Console.WriteLine("This is too High!");
+                y = Console.ReadLine();
+                z = Convert.ToInt32(y);
+            }
+            Console.WriteLine("This is correct! :)");
+            Console.ReadKey();
         }
     }
 }
